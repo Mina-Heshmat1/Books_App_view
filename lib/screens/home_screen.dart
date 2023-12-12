17fba67ccee32f80b24cc8d9be3a9aa5d14e1395
Mina.Heshmat <minaheshmat33@gmail.com>
2023-12-12 16:22:59 +0200

    to coummit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,5 @@
 
-
 import 'package:flutter/material.dart';
-
 import '../consttants.dart';
 import '../widgets/book_rating.dart';
 import '../widgets/reading_card_list.dart';
@@ -98,98 +96,98 @@ class HomeScreen extends StatelessWidget {
                           ),
                         ),
                         bestOfTheDayCard(size, context),
-                        RichText(
-                          text: const TextSpan(
-                            style: TextStyle(color: Colors.green),
-                            children: [
-                              TextSpan(text: "Continue "),
-                              TextSpan(
-                                text: "reading...",
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ),
+                        // RichText(
+                        //   text: const TextSpan(
+                        //     style: TextStyle(color: Colors.green),
+                        //     children: [
+                        //       TextSpan(text: "Continue "),
+                        //       TextSpan(
+                        //         text: "reading...",
+                        //         style: TextStyle(fontWeight: FontWeight.bold),
+                        //       ),
+                        //     ],
+                        //   ),
+                        // ),
                         const SizedBox(height: 20),
-                        Container(
-                          height: 80,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(38.5),
-                            boxShadow: [
-                              BoxShadow(
-                                offset: const Offset(0, 10),
-                                blurRadius: 33,
-                                color: const Color(0xFFD3D3D3).withOpacity(.84),
-                              ),
-                            ],
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(38.5),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Expanded(
-                                  child: Padding(
-                                    padding:
-                                        const EdgeInsets.only(left: 30, right: 20),
-                                    child: Row(
-                                      children: <Widget>[
-                                        const Expanded(
-                                          child: Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.end,
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: <Widget>[
-                                              Text(
-                                                "Crushing & Influence",
-                                                style: TextStyle(
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              ),
-                                              Text(
-                                                "Gary Venchuk",
-                                                style: TextStyle(
-                                                  color: kLightBlackColor,
-                                                ),
-                                              ),
-                                              Align(
-                                                alignment:
-                                                    Alignment.bottomRight,
-                                                child: Text(
-                                                  "Chapter 7 of 10",
-                                                  style: TextStyle(
-                                                    fontSize: 10,
-                                                    color: kLightBlackColor,
-                                                  ),
-                                                ),
-                                              ),
-                                              SizedBox(height: 5),
-                                            ],
-                                          ),
-                                        ),
-                                        Image.asset(
-                                          "assets/images/book-1.png",
-                                          width: 55,
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  height: 7,
-                                  width: size.width * .65,
-                                  decoration: BoxDecoration(
-                                    color: kProgressIndicator,
-                                    borderRadius: BorderRadius.circular(7),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
+                        // Container(
+                        //   height: 80,
+                        //   width: double.infinity,
+                        //   decoration: BoxDecoration(
+                        //     color: Colors.white,
+                        //     borderRadius: BorderRadius.circular(38.5),
+                        //     boxShadow: [
+                        //       BoxShadow(
+                        //         offset: const Offset(0, 10),
+                        //         blurRadius: 33,
+                        //         color: const Color(0xFFD3D3D3).withOpacity(.84),
+                        //       ),
+                        //     ],
+                        //   ),
+                        //   child: ClipRRect(
+                        //     borderRadius: BorderRadius.circular(38.5),
+                        //     child: Column(
+                        //       crossAxisAlignment: CrossAxisAlignment.start,
+                        //       children: <Widget>[
+                        //         Expanded(
+                        //           child: Padding(
+                        //             padding:
+                        //                 const EdgeInsets.only(left: 30, right: 20),
+                        //             child: Row(
+                        //               children: <Widget>[
+                        //                 const Expanded(
+                        //                   child: Column(
+                        //                     mainAxisAlignment:
+                        //                         MainAxisAlignment.end,
+                        //                     crossAxisAlignment:
+                        //                         CrossAxisAlignment.start,
+                        //                     children: <Widget>[
+                        //                       Text(
+                        //                         "Crushing & Influence",
+                        //                         style: TextStyle(
+                        //                           fontWeight: FontWeight.bold,
+                        //                         ),
+                        //                       ),
+                        //                       Text(
+                        //                         "Gary Venchuk",
+                        //                         style: TextStyle(
+                        //                           color: kLightBlackColor,
+                        //                         ),
+                        //                       ),
+                        //                       Align(
+                        //                         alignment:
+                        //                             Alignment.bottomRight,
+                        //                         child: Text(
+                        //                           "Chapter 7 of 10",
+                        //                           style: TextStyle(
+                        //                             fontSize: 10,
+                        //                             color: kLightBlackColor,
+                        //                           ),
+                        //                         ),
+                        //                       ),
+                        //                       SizedBox(height: 5),
+                        //                     ],
+                        //                   ),
+                        //                 ),
+                        //                 Image.asset(
+                        //                   "assets/images/book-1.png",
+                        //                   width: 55,
+                        //                 )
+                        //               ],
+                        //             ),
+                        //           ),
+                        //         ),
+                        //         Container(
+                        //           height: 7,
+                        //           width: size.width * .65,
+                        //           decoration: BoxDecoration(
+                        //             color: kProgressIndicator,
+                        //             borderRadius: BorderRadius.circular(7),
+                        //           ),
+                        //         ),
+                        //       ],
+                        //     ),
+                        //   ),
+                        // ),
                         const SizedBox(height: 40),
                       ],
                     ),
